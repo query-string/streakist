e02c0a616d987d292f93c0b7459e1e748757a3f6
Alex Timofeev <atimofeev@reactant.ru>
2016-11-09 10:27:35 +0700

    Be able to create authentications

--- apps/main/lib/main/persistence/repositories/authentications.rb
+++ apps/main/lib/main/persistence/repositories/authentications.rb
@@ -5,6 +5,8 @@ module Main
   module Persistence
     module Repositories
       class Authentications < Streakist::Repository[:authentications]
+        commands :create, update: [:by_id]
+
         def [](id)
           authentications.by_id(id).as(Entities::Authentication).one!
         end
